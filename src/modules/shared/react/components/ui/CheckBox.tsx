51853b7d1f64d27e2f30b7ec7893716f14001017
Lioruby <levy.lior.pro@gmail.com>
2024-07-21 18:08:02 +0200

    fix: save file

--- src/modules/shared/react/components/ui/CheckBox.tsx
+++ src/modules/shared/react/components/ui/CheckBox.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "lucide-react";
-import { cn } from "@root/modules/shared/react/libs/classNames";
+import { cn } from "@root/modules/shared/react/libs/cn";
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
